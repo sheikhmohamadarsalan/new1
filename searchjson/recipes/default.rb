276cc1c7f7717b8c4cbeed4fd79c4a2bc580d95f
Rasti <rastvaho@amazon.com>
2019-01-31 15:13:03 +1100

    second commit

--- searchjson/recipes/default.rb
+++ searchjson/recipes/default.rb
@@ -1,6 +1,8 @@
 
 phpserver = search(:node, "layers:php-app").first
-Chef::Log.info("**********The public IP address is: '#{phpserver[:ip]}'**********")
+
+Chef::Log.info("********** The app's URL is '#{node['deploy']['php_training']['scm']['repository']}' **********")
+Chef::Log.info("** PrivateIP : '#{node['opsworks']['layers']['php-app']['instances']['refresherday4nodeobjects']['private_ip']}' **")
 
 file "/tmp/ip_addresses" do
   content "#{phpserver[:ip]}"
